5ce0e3d566b27ff0cbc4f01683fe11bf55310c32
goshbello <44555654+goshbello@users.noreply.github.com>
2022-03-12 22:18:09 +0000

    add ajax and js to handle page not to refresh when searching for stock

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,14 +3,20 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-        render 'users/portfolio'
+        respond_to do |format|
+          format.js { render partial: 'users/result'}
+        end
       else
-        flash[:arlert] = "Please enter a valid symbol to search"
-        redirect_to portfolio_path
+        respond_to do |format|
+          flash.now[:arlert] = "Please enter a valid symbol to search"
+          format.js { render partial: 'users/result'}
+        end
       end
     else
-      flash[:arlert] = "Please enter a symbol to search"
-      redirect_to portfolio_path
+      respond_to do |format|
+        flash.now[:arlert] = "Please enter a symbol to search"
+        format.js { render partial: 'users/result'}
+      end
     end
   end
 end
